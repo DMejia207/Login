704eb1e5354a54d86e74664aa470d007f04d22aa
denis <denis@DESKTOP-ULFMESB>
2023-02-25 07:43:14 -0600

    Agregar archivos de proyecto.

---
+++ Login/Models/Info.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Login.Models
+{
+    public class Info
+    {
+        int id { get; set; }
+        public string nombre { get; set; }
+        public string apellidos { get; set; }
+        public string edad { get; set; }
+        public string correo { get; set; }
+    }
+}
---
+++ Login/Views/PageLogin.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Login.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageLogin : ContentPage
+    {
+        public PageLogin()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnmostrar_Clicked(object sender, EventArgs e)
+        {
+            var infor = new Models.Info
+            {
+                nombre = txtnombre.Text,
+                apellidos = txtapellidos.Text,
+                correo = txtcorreo.Text,
+                edad = cbedad.SelectedItem.ToString()
+            };
+
+
+            var page = new Views.PageCargInfo();
+            page.BindingContext = infor;
+            await Navigation.PushAsync(page);
+        }
+        }
+    }
